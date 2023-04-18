d6874a4a0f1b8b6772ff8883496da4da9dd3c448
AryanK1511 <aryankhurana1511@gmail.com>
2023-04-18 08:41:33 -0400

    Added more code

--- 000-Extras_and_Misc/003-Final-Exam-Prac-Ques-1.cpp
+++ 000-Extras_and_Misc/003-Final-Exam-Prac-Ques-1.cpp
@@ -105,3 +105,116 @@ int main() {
 
     return 0;
 }
+
+/*
+=== SOLUTION WITH SMART POINTERS ===
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <thread>
+#include <memory>
+#include <string>
+using namespace std;
+
+struct Gem {
+    std::string name;
+    std::string colour;
+    int shine;
+    bool polished;
+};
+
+class Box {
+private:
+    std::vector<std::shared_ptr<Gem>> m_gems {};
+public:
+    Box() = default;
+
+    Box& operator+=(const Gem& gem) {
+        std::shared_ptr<Gem> newGem(new Gem(gem));
+        m_gems.push_back(newGem);
+        return *this;
+    };
+
+    Box& operator-=(const string& name) {
+        size_t curSize = m_gems.size();
+        auto itr = std::remove_if(m_gems.begin(), m_gems.end(), [=](const shared_ptr<Gem>& g) {
+            return g->name == name;
+        });
+        m_gems.resize(itr - m_gems.begin());
+
+        if (m_gems.size() == curSize) {
+            throw std::string("No such gem found");
+        }
+
+        return *this;
+    }
+
+    void process() {
+        transform(m_gems.begin(), m_gems.end(), m_gems.begin(), [](shared_ptr<Gem>& gem) {
+            if (!gem->polished) {
+                gem->polished = true;
+                gem->shine += 20;
+            }
+            return gem;
+        });
+    }
+
+    void display() const {
+        for (auto& g: m_gems) {
+            cout << g->name << " | " << g->colour << " | " << g->shine << " | " << g->polished << endl;
+        }
+    }
+};
+
+int main() {
+    Box b;
+    cout << "========== Adding Gems ==========" << endl;
+    b += Gem{"Emerald", "Green", 20, false};
+    b += Gem{"Ruby", "Red", 50, true};
+    b += Gem{"Sapphire", "Blue", 80, false};
+    b += Gem{"Onyx", "Black", 60, false};
+    b += Gem{"Opal", "White", 10, false};
+    b.display();
+    cout << "=================================\n" << endl;
+
+    cout << "========== Removing Gems ==========" << endl;
+    try {
+        b -= "Ruby";
+        b -= "Emerald";
+        b -= "Diamond";
+        b.display();
+    }
+    catch (string c) {
+        cout << c << endl;
+    }
+    cout << "===================================\n" << endl;
+
+    cout << "========== Polishing Gems ==========" << endl;
+    b.process();
+    b.display();
+    cout << "====================================\n" << endl;
+
+    cout << "========== Vector of Boxes ==========" << endl;
+    vector<Box*> boxes;
+    boxes.push_back(new Box());
+    boxes.push_back(new Box());
+    boxes.push_back(new Box());
+
+    for (auto x : boxes) {
+        x->operator+=(Gem{ "Emerald", "Green", 20, false });
+        x->operator+=(Gem{ "Ruby", "Red", 90, false });
+        x->operator+=(Gem{ "Sapphire", "Blue", 40, false });
+    }
+
+    for (auto& x: boxes) {
+        x->display();
+    }
+    cout << "=====================================" << endl;
+
+    for (auto x : boxes) {
+        delete x;
+    }
+
+    return 0;
+}
+*/
